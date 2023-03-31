fdf16491bd8af3e90fbdbd3308d7e42f573c4a27
shobannah <116910257+shobannah@users.noreply.github.com>
2023-03-31 11:55:31 -0400

    updated readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# Tech Blog
+
+
+## Description
+
+This is a CMS-style blog site similar to a Wordpress site, where developers can publish their blog posts.
+
+## Screenshot
+
+![image](./assets/images/Screenshot.png)
+
+
+
+## Link
+
+<a href=https://techblogchallenge-app.herokuapp.com> 
--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -15,26 +15,6 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
-router.put('/:id/edit', async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const updatedForm = await Project.update({
-      ...req.body,
-      }, { where: { id } });
-
-    console.log(updatedForm[0])
-
-    if (updatedForm[0] === 0) {
-      return res.status(404).json({ error: 'Form not found' });
-    }
-
-    return res.status(200).json({ message: 'Form updated successfully' });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: 'Unable to update form' });
-  }
-});
 
 
 router.delete('/:id', withAuth, async (req, res) => {
